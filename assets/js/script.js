d464f9565e112c86239b68cbd80b6159bae8cce6
sanam1060 <sanam_g1990@yahoo.com>
2022-08-30 16:55:19 -0400

    add questions to js; finish the homepage index.html and add class names and ids

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,49 @@
+// Arrays of questions/answers
+let questions = [
+  {
+    question: "What year was the very first model of the iPhone released?",
+    answerA: "1. 1997",
+    answerB: "2. 2005",
+    answerC: "3. 2007",
+    answerD: "4. 1995",
+    correctAnswer: "c",
+  },
+  {
+    question: "Which email service is owned by Microsoft?",
+    answerA: "1. iCloud",
+    answerB: "2. AOL",
+    answerC: "3. Yahoo",
+    answerD: "4. Hotmail",
+    correctAnswer: "d",
+  },
+  {
+    question: "Arrays in JavaScript can be used to store _______.",
+    answerA: "1. numbers and strings",
+    answerB: "2. other arrays",
+    answerC: "3. booleans",
+    answerD: "4. all of the above",
+    correctAnswer: "d",
+  },
+  {
+    question: "What is often seen as the smallest unit of memory?",
+    answerA: "1. Byte",
+    answerB: "2. Bit",
+    answerC: "3. Kilobyte",
+    answerD: "4. Kilobit",
+    correctAnswer: "a",
+  },
+  {
+    question: "How many generations of computers have been invented?",
+    answerA: "1. Seven generations",
+    answerB: "2. Four generations",
+    answerC: "3. Six generations",
+    answerD: "4. Five generations",
+    correctAnswer: "d",
+  },
+];
+
 // DOM elements used in this project:
 var buttonEl = document.querySelector(".btn-start-quiz");
-
 
 var startTimer = function () {
   var timerEl = document.createElement("div");
@@ -8,4 +51,8 @@ var startTimer = function () {
   tasksToDoEl.appendChild(listItemEl);
 };
 
-buttonEl.addEventListener("click", startQuiz);
+// DOM elements
+
+// timer function
+
+// addEventListeners - click
